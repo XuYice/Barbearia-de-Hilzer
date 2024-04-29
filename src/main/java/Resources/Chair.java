3dfdc229fe0c8f69bc575f6d763264bb65c2c5d6
Joilson de Oliveira Telles <joilsontelles@gmail.com>
2024-04-28 23:05:20 -0300

    FEAT: Add Chair and Sofa class

---
+++ src/main/java/Resources/Chair.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Resources;
+
+import Personas.Barber;
+import Personas.Custumer;
+
+/**
+ *
+ * @author joilson
+ */
+public class Chair {
+    boolean status;
+    Barber barber;
+    Custumer custumer;
+
+    public Chair() {
+    }
+    
+    public void Use(){
+    }
+    
+    public void GetUp(){
+    }
+}
---
+++ src/main/java/Resources/Sofa.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Resources;
+
+import Personas.Custumer;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author joils
+ */
+public class Sofa {
+    boolean status;
+    ArrayList<Custumer> custumerList;
+
+    public Sofa() {
+    }
+    
+    public void Use(){
+    }
+    
+    public void GetUp(){
+    }
+}
