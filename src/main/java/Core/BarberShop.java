4f4e100f78cf28b42017ead57e600bbb4aec5bb2
Joilson de Oliveira Telles <joilsontelles@gmail.com>
2024-04-28 23:18:33 -0300

    FEAT: changes

--- src/main/java/Core/BarberShop.java
+++ src/main/java/Core/BarberShop.java
@@ -1,5 +1,11 @@
 package Core;
 
+import Personas.Barber;
+import Personas.Custumer;
+import Resources.Chair;
+import Resources.Sofa;
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
@@ -11,7 +17,21 @@ package Core;
  * @author joils
  */
 public class BarberShop {
+    Sofa sofa;
+    ArrayList<Chair> chairList;
+    ArrayList<Barber> barberList;
+    ArrayList<Custumer> custumerList;
+    boolean POS;
 
+    public void Relocate(){
+    }
+    
+    public void Payment(){
+    }
+    
+    public void Verify(){
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
