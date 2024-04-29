ab10d292c5ccbfd6f36c8c2f24eecc7d9627e099
Joilson de Oliveira Telles <joilsontelles@gmail.com>
2024-04-28 21:04:29 -0300

    first commit

---
+++ src/main/java/br/feevale/feevale/processamentoparalelo/threads/v1/FeevaleProcessamentoParaleloThreadsV1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.feevale.feevale.processamentoparalelo.threads.v1;
+
+/**
+ *
+ * @author joils
+ */
+public class FeevaleProcessamentoParaleloThreadsV1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
