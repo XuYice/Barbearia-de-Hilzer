193bb155d4c51440c12f2500cc0c76737363e427
Hiago Silva <hiagoluansilva@gmail.com>
2024-05-01 15:18:31 -0300

    Adicionados métodos Auxiliares BarberShop

--- src/main/java/Core/BarberShop.java
+++ src/main/java/Core/BarberShop.java
@@ -23,6 +23,38 @@ public class BarberShop {
     ArrayList<Custumer> custumerList;
     boolean POS;
 
+    public ArrayList<Chair> getChairList() {
+        return chairList;
+    }
+
+    public void setChairList(ArrayList<Chair> chairList) {
+        this.chairList = chairList;
+    }
+
+    public ArrayList<Barber> getBarberList() {
+        return this.barberList;
+    }
+
+    public void setBarberList(ArrayList<Barber> barberList) {
+        this.barberList = barberList;
+    }
+
+    public ArrayList<Custumer> getCustumerList() {
+        return custumerList;
+    }
+
+    public void setCustumerList(ArrayList<Custumer> custumerList) {
+        this.custumerList = custumerList;
+    }
+
+    public boolean getPOS() {
+        return POS;
+    }
+
+    public void setPOS(boolean pOS) {
+        POS = pOS;
+    }
+
     public void Relocate(){
     }
     
@@ -33,6 +65,28 @@ public class BarberShop {
     }
     
     public static void main(String[] args) {
+
+        BarberShop bS = new BarberShop();
+        ArrayList<Barber> barberList = new ArrayList<Barber>();
+        bS.setBarberList(barberList);
+        ArrayList<Chair> chairList = new ArrayList<Chair>();
+        bS.setChairList(chairList);
+                
+        Barber b0 = new Barber("thBarber0");
+        barberList.add(b0);
+        Barber b1 = new Barber("thBarber1");
+        barberList.add(b1);
+        Barber b2 = new Barber("thBarber2");
+        barberList.add(b2);
+        
+
+        Chair c0 = new Chair();
+        chairList.add(c0);
+        Chair c1 = new Chair();
+        chairList.add(c1);
+        Chair c2 = new Chair();
+        chairList.add(c2);
+        
         System.out.println("Hello World!");
     }
 }
