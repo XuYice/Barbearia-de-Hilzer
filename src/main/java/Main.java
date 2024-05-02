b18eb7dffd431286d89b3741862f14ac6d494dc3
Hiago Silva <hiagoluansilva@gmail.com>
2024-05-01 21:11:37 -0300

    CHANGES

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 import Core.BarberShop;
 import Personas.Barber;
+import Personas.Custumer;
 import Resources.Chair;
 import java.util.ArrayList;
 
 public class Main {
+
+
+    public Custumer generateClient(){
+
+        Custumer newCustumer = new Custumer(null, b);
+        System.out.println("New custumer has arrive");     
+        return newCustumer;     
+    }
+
      public static void main(String[] args) {
 
         BarberShop bS = new BarberShop();
@@ -19,13 +29,14 @@ public class Main {
         Barber b2 = new Barber("thBarber2");
         barberList.add(b2);
 
-
         Chair c0 = new Chair();
         chairList.add(c0);
         Chair c1 = new Chair();
         chairList.add(c1);
         Chair c2 = new Chair();
         chairList.add(c2);
+
+
 
         System.out.println("Hello World!");
     }
