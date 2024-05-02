4bb3b83839a4e896c46684512cae7d350128b7c0
Joilson de Oliveira Telles <joilsontelles@gmail.com>
2024-05-02 01:10:06 -0300

    FEAT: changes

--- src/main/java/Resources/Sofa.java
+++ src/main/java/Resources/Sofa.java
@@ -5,16 +5,9 @@ import java.util.ArrayList;
 
 public class Sofa {
 
-    public boolean status;
     public ArrayList<Custumer> custumerList;
 
     public Sofa() {
         this.custumerList = new ArrayList<Custumer>();
-    }
-
-    public void Use() {
-    }
-
-    public void GetUp() {
     }
 }
